53800939a10cecba766f92551ad51d7ffb293d06
HariEdith <elanhariharan@gmail.com>
2022-10-14 19:18:12 +0530

    update

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -2,7 +2,7 @@ import {
     Link
 } from "react-router-dom";
 import { Navbar, Nav, Button, Container } from 'react-bootstrap'
-import market from './logo.jpg'
+import market from './logo.png'
 
 const Navigation = ({ web3Handler, account }) => {
     return (
